08bd0059edca040a4fe8b94e9507b574b39f53df
Souranil Sen <souranil@unity3d.com>
2022-02-18 14:20:33 -0800

    Bumping version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ URL = "https://https://github.com/Unity-Technologies/unity-vision"
 EMAIL = "souranil@unity3d.com"
 AUTHOR = "Souranil Sen"
 REQUIRES_PYTHON = ">=3.6"
-VERSION = "0.1.6"
+VERSION = "0.1.7"
 
 
 here = os.path.abspath(os.path.dirname(__file__))
