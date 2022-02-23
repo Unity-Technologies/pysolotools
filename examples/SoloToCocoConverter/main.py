781c7737282d5e1a06adb02ae5c95def7d0fff9f
Ruiyu Zhnag <etain42@gmail.com>
2022-02-22 17:54:28 -0800

    remove unused dataset and update main

--- examples/SoloToCocoConverter/main.py
+++ examples/SoloToCocoConverter/main.py
@@ -1,8 +1,11 @@
+import os
+from pathlib import Path
+
 from solo_to_coco import COCOInstancesTransformer as Solo_COCOInstancesTransformer
 
 def run_solo():
-    dataset = Solo_COCOInstancesTransformer(data_root=r"C:\Users\ruiyu.zhang\Desktop\legacyToCoco\solo_1kpt")
-    output = dataset.execute(output=r"C:\Users\ruiyu.zhang\Desktop\legacyToCoco\output_solo_1kpt")
+    dataset = Solo_COCOInstancesTransformer(data_root=os.path.join(Path(__file__).parents[1], "data","solo"))
+    output = dataset.execute(output=os.path.join(Path(__file__).parents[1], "data_output"))
     print(output)
 
 if __name__ == "__main__":
