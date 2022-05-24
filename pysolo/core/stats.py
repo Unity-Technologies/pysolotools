129b0c640186acd02519e0f2e772892989a2f521
Saurav Dhakad <saurav.dhakad@saurav.dhakad-2Y8M>
2022-05-24 15:32:42 -0700

    stats fix

--- pysolo/core/stats.py
+++ pysolo/core/stats.py
@@ -3,7 +3,8 @@ from typing import List
 import numpy as np
 
 from pysolo.consumers.solo.parser import Solo
-from pysolo.core.models.solo import BoundingBox2DAnnotation
+from pysolo.core.models.solo import (BoundingBox2DAnnotation,
+                                     BoundingBox2DAnnotationDefinition)
 
 
 class SoloStats:
@@ -13,6 +14,13 @@ class SoloStats:
 
     def _reset_frame_idx(self):
         self.solo.frame_idx = 0
+
+    def get_categories(self):
+        categories = {}
+        for res in filter(lambda ann_def: isinstance(ann_def, BoundingBox2DAnnotationDefinition), self.solo.annotation_definitions):
+            for s in res.spec:
+                categories[s.label_id] = s.label_name
+        return categories
 
     def get_frame_ids(self):
         ids = []
@@ -33,14 +41,13 @@ class SoloStats:
             try:
                 frame = next(self.solo)
                 for f in frame.captures:
-                    for k in f.annotations:
-                        if isinstance(k, BoundingBox2DAnnotation):
-                            for v in k.values:
-                                if cat_ids:
-                                    if v.labelId in cat_ids:
-                                        num_bbox += 1
-                                else:
-                                    num_bbox += 1
+                    for ann in filter(
+                            lambda k: isinstance(k, BoundingBox2DAnnotation),
+                            f.annotations):
+                        annotations = list(filter(
+                                lambda x: cat_ids is None or x.labelId in cat_ids,
+                                ann.values))
+                        num_bbox += len(annotations)
             except StopIteration:
                 break
         self._reset_frame_idx()
@@ -53,19 +60,17 @@ class SoloStats:
             try:
                 frame = next(self.solo)
                 for f in frame.captures:
-                    for k in f.annotations:
-                        if isinstance(k, BoundingBox2DAnnotation):
-                            num_bbox = 0
-                            for v in k.values:
-                                if cat_ids:
-                                    if v.labelId in cat_ids:
-                                        num_bbox += 1
-                                else:
-                                    num_bbox += 1
-                            if num_bbox in bbox_dist.keys():
-                                bbox_dist[num_bbox] += 1
-                            else:
-                                bbox_dist[num_bbox] = 1
+                    for ann in filter(
+                            lambda k: isinstance(k, BoundingBox2DAnnotation),
+                            f.annotations):
+                        annotations = list(filter(
+                                lambda x: cat_ids is None or x.labelId in cat_ids,
+                                ann.values))
+                        num_bbox = len(annotations)
+                        if num_bbox in bbox_dist.keys():
+                            bbox_dist[num_bbox] += 1
+                        else:
+                            bbox_dist[num_bbox] = 1
             except StopIteration:
                 break
 
@@ -84,16 +89,15 @@ class SoloStats:
                     if bbox_heatmap is None:
                         w, h = int(f.dimension[0]), int(f.dimension[1])
                         bbox_heatmap = np.zeros([h, w, 1])
-                    for k in f.annotations:
-                        if isinstance(k, BoundingBox2DAnnotation):
-                            for v in k.values:
-                                bbox = [int(v.origin[0]), int(v.origin[1]),
-                                        int(v.dimension[0]), int(v.dimension[1])]
-                                if cat_ids:
-                                    if v.labelId in cat_ids:
-                                        bbox_heatmap[bbox[1]: bbox[1] + bbox[3], bbox[0]: bbox[0] + bbox[2], :] += 1
-                                else:
-                                    bbox_heatmap[bbox[1]: bbox[1] + bbox[3], bbox[0]: bbox[0] + bbox[2], :] += 1
+                    for ann in filter(
+                            lambda k: isinstance(k, BoundingBox2DAnnotation),
+                            f.annotations):
+                        for v in filter(
+                                lambda x: cat_ids is None or x.labelId in cat_ids,
+                                ann.values):
+                            bbox = [int(v.origin[0]), int(v.origin[1]),
+                                    int(v.dimension[0]), int(v.dimension[1])]
+                            bbox_heatmap[bbox[1]: bbox[1] + bbox[3], bbox[0]: bbox[0] + bbox[2], :] += 1
 
             except StopIteration:
                 break
@@ -108,15 +112,14 @@ class SoloStats:
                 for f in frame.captures:
                     w, h = f.dimension[0], f.dimension[1]
                     img_area = w * h
-                    for k in f.annotations:
-                        if isinstance(k, BoundingBox2DAnnotation):
-                            for v in k.values:
-                                bbox_area = v.dimension[0] * v.dimension[1]
-                                if cat_ids:
-                                    if v.labelId in cat_ids:
-                                        bbox_relative_size.append(np.sqrt(bbox_area / img_area))
-                                else:
-                                    bbox_relative_size.append(np.sqrt(bbox_area / img_area))
+                    for ann in filter(
+                            lambda k: isinstance(k, BoundingBox2DAnnotation),
+                            f.annotations):
+                        for v in filter(
+                                lambda x: cat_ids is None or x.labelId in cat_ids,
+                                ann.values):
+                            bbox_area = v.dimension[0] * v.dimension[1]
+                            bbox_relative_size.append(np.sqrt(bbox_area / img_area))
 
             except StopIteration:
                 break
