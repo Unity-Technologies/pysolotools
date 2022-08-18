f7d4f6374f4dfe75ef9e8031e7c4bd9c2c9173c4
sanjay <86sanj@gmail.com>
2022-08-18 16:39:40 -0700

    convertor unit test cases

--- tests/converters/test_solo2coco_convertor.py
+++ tests/converters/test_solo2coco_convertor.py
@@ -1,3 +1,4 @@
+import os
 import tempfile
 from pathlib import Path
 from unittest.mock import MagicMock, patch
@@ -7,19 +8,21 @@ import pytest
 
 from pysolotools.consumers import Solo
 from pysolotools.converters import SOLO2COCOConverter
+from pysolotools.core.models import RGBCameraCapture
+
+pozole_sample_data = os.path.join(
+    Path(__file__).parents[1], "data", "format_output_by_pozole", "attempt0"
+)
+
+mysterious_sample_data = os.path.join(
+    Path(__file__).parents[1], "data", "mysterious_format_no_idea_the_source", "solo"
+)
 
 
 @pytest.fixture
 def solo2coco_instance():
     mock_solo2coco = SOLO2COCOConverter()
     return mock_solo2coco
-
-
-@pytest.fixture
-def solo_instance():
-    solo_path = "tests/mock_data/solo"
-    mock_solo = Solo(solo_path)
-    return mock_solo
 
 
 def test_create_ann_file():
@@ -112,3 +115,168 @@ def test_process_annotations(
     assert (
         mock_load_segmentation_image.call_count == 2
     )  # called in both instance and semantic seg
+
+
+@pytest.mark.parametrize(
+    "input_data_path, metadata_file_path, annotation_definitions_file_path",
+    [
+        (
+            pozole_sample_data,
+            os.path.join(pozole_sample_data, "metadata", "metadata.json"),
+            os.path.join(pozole_sample_data, "metadata", "annotation_definitions.json"),
+        ),
+        (mysterious_sample_data, None, None),
+    ],
+)
+def test_categories(
+    input_data_path,
+    metadata_file_path,
+    annotation_definitions_file_path,
+    solo2coco_instance,
+):
+
+    solo = Solo(
+        data_path=input_data_path,
+        metadata_file=metadata_file_path,
+        annotation_definitions_file=annotation_definitions_file_path,
+    )
+    solo2coco_instance._solo = solo
+    return_value = solo2coco_instance._categories()
+
+    expected = [
+        {
+            "id": 1,
+            "name": "Crate",
+            "supercategory": "default",
+            "keypoints": [],
+            "skeleton": [],
+        },
+        {
+            "id": 2,
+            "name": "Cube",
+            "supercategory": "default",
+            "keypoints": [],
+            "skeleton": [],
+        },
+        {
+            "id": 3,
+            "name": "Box",
+            "supercategory": "default",
+            "keypoints": [],
+            "skeleton": [],
+        },
+        {
+            "id": 4,
+            "name": "Terrain",
+            "supercategory": "default",
+            "keypoints": [],
+            "skeleton": [],
+        },
+        {
+            "id": 5,
+            "name": "Character",
+            "supercategory": "default",
+            "keypoints": [],
+            "skeleton": [],
+        },
+    ]
+
+    assert return_value == expected
+
+
+@patch("pysolotools.converters.solo2coco.SOLO2COCOConverter._categories")
+@patch("pysolotools.converters.solo2coco.SOLO2COCOConverter._create_ann_file")
+def test_write_out_annotations(
+    mock_create_ann_file, mock_categories, solo2coco_instance
+):
+    output_dir = "test"
+    solo2coco_instance._write_out_annotations(output_dir)
+    mock_categories.assert_called_once()
+    mock_create_ann_file.assert_called_once()
+
+
+@patch("pysolotools.converters.solo2coco.SOLO2COCOConverter._process_annotations")
+@patch("pysolotools.converters.solo2coco.SOLO2COCOConverter._process_rgb_image")
+def test_process_instances(mock_process_rgb_image, mock_process_annotations):
+    frame = MagicMock(spec=RGBCameraCapture)
+    frame.sequence = []
+    frame.captures = [MagicMock(spec=RGBCameraCapture)]
+    idx = "test"
+    output = "test"
+    data_root = "test"
+    solo_kp_map = MagicMock()
+
+    mock_process_annotations.return_value = (None, None, None)
+    SOLO2COCOConverter._process_instances(frame, idx, output, data_root, solo_kp_map)
+    mock_process_rgb_image.assert_called_once()
+    mock_process_annotations.assert_called_once()
+
+
+@pytest.mark.parametrize(
+    "input_data_path, metadata_file_path, annotation_definitions_file_path",
+    [
+        (
+            pozole_sample_data,
+            os.path.join(pozole_sample_data, "metadata", "metadata.json"),
+            os.path.join(pozole_sample_data, "metadata", "annotation_definitions.json"),
+        ),
+        (mysterious_sample_data, None, None),
+    ],
+)
+def test_get_solo_kp_map(
+    input_data_path,
+    metadata_file_path,
+    annotation_definitions_file_path,
+    solo2coco_instance,
+):
+    solo = Solo(
+        data_path=input_data_path,
+        metadata_file=metadata_file_path,
+        annotation_definitions_file=annotation_definitions_file_path,
+    )
+    solo2coco_instance._solo = solo
+
+    resturn_value = solo2coco_instance._get_solo_kp_map()
+
+    expected = {
+        0: "nose",
+        1: "neck",
+        2: "right_shoulder",
+        3: "right_elbow",
+        4: "right_wrist",
+        5: "left_shoulder",
+        6: "left_elbow",
+        7: "left_wrist",
+        8: "right_hip",
+        9: "right_knee",
+        10: "right_ankle",
+        11: "left_hip",
+        12: "left_knee",
+        13: "left_ankle",
+        14: "right_eye",
+        15: "left_eye",
+        16: "right_ear",
+        17: "left_ear",
+    }
+
+    assert resturn_value == expected
+
+
+def test_callback(solo2coco_instance):
+    result = [["image"], ["box"], ["instance"], ["semantic"]]
+    solo2coco_instance.callback(result)
+
+    assert solo2coco_instance._bbox_annotations == ["box"]
+    assert solo2coco_instance._instance_annotations == ["instance"]
+    assert solo2coco_instance._semantic_annotations == ["semantic"]
+
+
+@patch("pysolotools.converters.solo2coco.SOLO2COCOConverter._write_out_annotations")
+@patch("pysolotools.converters.solo2coco.SOLO2COCOConverter._get_solo_kp_map")
+def test_convert(mock_get_solo_kp_map, mock_write_out_annotations, solo2coco_instance):
+    arguments = {"output_path": "test"}
+    solo2coco_instance._pool = MagicMock()
+    solo2coco_instance._pool = MagicMock()
+    solo2coco_instance.convert(MagicMock(), arguments)
+    mock_get_solo_kp_map.assert_called_once()
+    mock_write_out_annotations.assert_called_once()
