cb045c2c54c5a3a8f4c37b8195768345204c85d9
Drew F <>
2022-08-23 09:01:59 -0500

    chore: fix all these broken tests

--- tests/converters/test_solo2coco.py
+++ tests/converters/test_solo2coco.py
@@ -2,40 +2,14 @@ import os
 import tempfile
 from pathlib import Path
 
-import pytest
-
 from pysolotools.consumers.solo import Solo
 from pysolotools.converters import SOLO2COCOConverter
 
-pozole_sample_data = os.path.join(
-    Path(__file__).parents[1], "data", "format_output_by_pozole", "attempt0"
-)
 
-mysterious_sample_data = os.path.join(
-    Path(__file__).parents[1], "data", "mysterious_format_no_idea_the_source", "solo"
-)
-
-
-@pytest.mark.parametrize(
-    "input_data_path, metadata_file_path, annotation_definitions_file_path",
-    [
-        (
-            pozole_sample_data,
-            os.path.join(pozole_sample_data, "metadata", "metadata.json"),
-            os.path.join(pozole_sample_data, "metadata", "annotation_definitions.json"),
-        ),
-        (mysterious_sample_data, None, None),
-    ],
-)
-def test_solo2coco_create_files(
-    input_data_path, metadata_file_path, annotation_definitions_file_path
-):
+def test_solo2coco_create_files():
+    input_data_path = os.path.join(Path(__file__).parents[1], "data", "solo")
     converter = SOLO2COCOConverter()
-    solo = Solo(
-        data_path=input_data_path,
-        metadata_file=metadata_file_path,
-        annotation_definitions_file=annotation_definitions_file_path,
-    )
+    solo = Solo(data_path=input_data_path)
 
     with tempfile.TemporaryDirectory() as tmp_dir:
         converter.convert(solo, {"output_path": tmp_dir})
