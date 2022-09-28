24f3367d1192e3a7a0b68819d1c77ffb55263e7c
Drew F <>
2022-09-28 18:17:17 -0500

    fix: trying to make somethings useable

--- tests/stats/analyzers/test_human_annotation_analyzer.py
+++ tests/stats/analyzers/test_human_annotation_analyzer.py
@@ -6,10 +6,30 @@ from pysolotools.stats.handler import StatsHandler
 
 
 def test_foo():
+    def print_thing(key: str, source: dict):
+        print(f"{key}: {source.get(key, {}).get('count', 0)}")
+
     solo = Solo(
         data_path="/Users/drew.flintosh/Library/Application Support/DefaultCompany/test-palette-2/solo"
     )
     handler = StatsHandler(solo=solo)
     analyzers = [HumanMetadataAnnotationAnalyzer()]
     result = handler.handle(analyzers=analyzers)
-    print(result)
+    analyzer = result.get("HumanMetadataAnnotationAnalyzer")
+    things_i_care_about = [
+        "human",
+        "Adult",
+        "Toddler",
+        "Female",
+        "Male",
+        "African",
+        "Asian",
+        "Caucasian",
+        "LatinAmerican",
+        "MiddleEastern",
+    ]
+    list(map(lambda x: print_thing(x, analyzer), things_i_care_about))
+
+    for k, v in analyzer.items():
+        if v.get("values", None):
+            print(k)
