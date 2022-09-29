353136351c0d7ee0aeb1ff8f6858c8e4e3d003e4
Drew F <>
2022-09-29 16:12:06 -0500

    test: my coverage was poor, fix that

--- tests/stats/analyzers/test_human_metadata_analyzer.py
+++ tests/stats/analyzers/test_human_metadata_analyzer.py
@@ -1,11 +1,16 @@
-from unittest.mock import create_autospec, patch
+from unittest.mock import Mock, create_autospec, patch
 
 import pytest
 
 from pysolotools.core import Frame
-from pysolotools.core.models import HumanMetadataAnnotation, HumanMetadataLabel
+from pysolotools.core.models import (
+    HumanMetadataAnnotation,
+    HumanMetadataLabel,
+    NormalAnnotation,
+)
 from pysolotools.stats.analyzers.human_metadata_analyzer import (
     AggregateObject,
+    HumanMetadataAnnotationAggregate,
     HumanMetadataAnnotationAnalyzer,
 )
 
@@ -60,6 +65,16 @@ class TestHumanMetadataAnnotation:
         analyzer.merge(frame_result=test_input)
         assert analyzer._res.statistics.get("foo") == expected
 
+    def test_filter(self):
+        mock_frame = create_autospec(spec=Frame)
+        mock_human_metadata = create_autospec(spec=HumanMetadataAnnotation)
+        mock_other_annotation = create_autospec(spec=NormalAnnotation)
+        mock_capture = Mock()
+        mock_capture.annotations = [mock_human_metadata, mock_other_annotation]
+        mock_frame.captures = [mock_capture]
+        analyzer = HumanMetadataAnnotationAnalyzer()
+        assert analyzer._filter(frame=mock_frame) == [mock_human_metadata]
+
     @patch(
         "pysolotools.stats.analyzers.human_metadata_analyzer.HumanMetadataAnnotationAggregate",
         autospec=True,
@@ -105,3 +120,12 @@ class TestAggregateObject:
     def test_add(self, test_input, value, expected):
         result = test_input.add(value=value)
         assert result == expected
+
+
+class TestHumanMetadataAnnotationAggregate:
+    def test_to_dict(self):
+        test_input = {"foo": AggregateObject(key="bar")}
+        result = HumanMetadataAnnotationAggregate(statistics=test_input)
+        assert result.to_dict() == {
+            "foo": {"count": 0, "key": "bar", "sum": None, "value": None}
+        }
